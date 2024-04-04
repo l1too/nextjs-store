ce6a8d13aadb54becc528c824c2d5aa6e9b4dc7c
Gabriel Quiroga <gabrielnahuel.quiroga@gmail.com>
2024-04-04 13:58:54 -0300

    a

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 export const MainProducts = ()=> {
+    console.log(process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME);
+    
     return(
         <section>
             <h1>MainProducts</h1>
